b7f5add620ffa6be0ca9ccb83b7bb6ba80ec9f81
sentinel20 <emil.begaliev@gmail.com>
2022-07-07 20:08:19 +0600

    fixed

--- main.py
+++ main.py
@@ -1,52 +1,16 @@
 print('Hello my name is Sanzhar!')
-<<<<<<< HEAD
-
-=======
 print("hello tima!!")
-
-
 print('Hi, my name is Janylai!')
 print('Raimbek')
-
 print('Hello Im Ramazan')
-
 print('Hello Bema!!!')
->>>>>>> de0bcb9838f40f39ddfd62955bdaf9468e4e4c5b
-
-
-
-
-<<<<<<< HEAD
-
-<<<<<<< HEAD
-
-=======
 print('My name is Igor!')
 print('My name is Tariel')
-
-=======
 print('Hello my name is Talgat')
->>>>>>> 440e6f2c6c938d3785c196120a0eb64f6721fc9b
 print('Hello my name is Islam')
->>>>>>> de0bcb9838f40f39ddfd62955bdaf9468e4e4c5b
-
-
-
-
-<<<<<<< HEAD
-=======
 print('Hello my name is Nurmuhamed.')
->>>>>>> de0bcb9838f40f39ddfd62955bdaf9468e4e4c5b
-
 print('Hello my name is Manas')
-
-
-
-<<<<<<< HEAD
 print('My name Tariel')
-=======
 print('Hello my name is Aibek!')
 print('My name is Muha')
 print('My name is Adiltay')
-
->>>>>>> de0bcb9838f40f39ddfd62955bdaf9468e4e4c5b
