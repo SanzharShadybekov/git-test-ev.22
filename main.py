af439c7b2ad98925b041dd8f20ec6eb7481b592c
SanzharShadybekov <Shadubekovs777@gmail.com>
2022-07-07 20:41:14 +0600

    asd

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 print('Hello my name is Sanzhar!')
 print("hello tima!!")
-
+sadasdas
 print('Hello Bema!!!')
-
-
-
-
+sfdfsd
+sdfsdfsdf
+sdfsdfds
+sdfsdfsd
 print('My name is Igor!')
-
-
+dsfsdfds
+sdfsd
 print('Hello my name is Islam')
-
-
-
-
+sdfsdfsd
+sdfsdfs
+sdfsdfsdf
+sdfsdfsd
 print('Hello my name is Nurmuhamed.')
-
-
-
-
-
+sdfsdfsd
+sdfsdfsd
+dsfsdfsd
+sdfsdfsdfs
+dsfsdfsd
 print('Hello my name is Aibek!')
 print('My name is Muha')
 
