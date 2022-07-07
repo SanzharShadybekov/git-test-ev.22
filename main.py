8ef31776f6ac524d4ee4a7523c081447ea7dfa96
humster <park.ksenia23@gmail.com>
2022-07-07 20:10:59 +0600

    .

--- main.py
+++ main.py
@@ -1,85 +1,22 @@
 print('Hello my name is Sanzhar!')
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> 2be6ccc6fae42c3af3ffa5c795d0ab79ef0d8fd3
-=======
 print("hello tima!!")
->>>>>>> f83ccb2b1b04c0a88b05f9311e4d8400093c9cb7
-
-
-=======
 print("hello tima!!")
->>>>>>> b7f5add620ffa6be0ca9ccb83b7bb6ba80ec9f81
 print('Hi, my name is Janylai!')
 print('Raimbek')
 print('Hello Im Ramazan')
 print('Hello Bema!!!')
 print('My name is Igor!')
 print('My name is Tariel')
-<<<<<<< HEAD
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 print('Hello my name is Ksenia!')
-=======
->>>>>>> 2be6ccc6fae42c3af3ffa5c795d0ab79ef0d8fd3
-=======
-=======
->>>>>>> b7f5add620ffa6be0ca9ccb83b7bb6ba80ec9f81
 print('Hello my name is Talgat')
 print('Hello my name is Islam')
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> cd0fb10e68969c63d9adba2373d94ac88512e842
-=======
->>>>>>> de0bcb9838f40f39ddfd62955bdaf9468e4e4c5b
->>>>>>> 2be6ccc6fae42c3af3ffa5c795d0ab79ef0d8fd3
-
-
-
-=======
->>>>>>> f83ccb2b1b04c0a88b05f9311e4d8400093c9cb7
-
-<<<<<<< HEAD
-=======
-=======
->>>>>>> b7f5add620ffa6be0ca9ccb83b7bb6ba80ec9f81
 print('Hello my name is Nurmuhamed.')
 print('Hello my name is Manas')
-<<<<<<< HEAD
-
-
-
-<<<<<<< HEAD
-
-
-
-<<<<<<< HEAD
-=======
 print('Hello my name is Aibek!')
 print('My name is Muha')
-<<<<<<< HEAD
->>>>>>> f83ccb2b1b04c0a88b05f9311e4d8400093c9cb7
-=======
-
-
->>>>>>> cd0fb10e68969c63d9adba2373d94ac88512e842
-=======
-<<<<<<< HEAD
-=======
->>>>>>> b7f5add620ffa6be0ca9ccb83b7bb6ba80ec9f81
 print('My name Tariel')
 print('Hello my name is Aibek!')
 print('My name is Muha')
-<<<<<<< HEAD
 print('My name is Adiltay')
 
->>>>>>> de0bcb9838f40f39ddfd62955bdaf9468e4e4c5b
->>>>>>> 2be6ccc6fae42c3af3ffa5c795d0ab79ef0d8fd3
-=======
 print('My name is Adiltay')
->>>>>>> b7f5add620ffa6be0ca9ccb83b7bb6ba80ec9f81
