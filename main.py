b89e4dbcdaa1cad4fca9d9e01ab76c14e128b026
adiltai <adiltay90@mail.ru>
2022-07-07 19:54:16 +0600

    asd

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 print('Hello my name is Sanzhar!')
-<<<<<<< HEAD
-=======
 print("hello tima!!")
->>>>>>> a230cdaed82c88bf6badeb3e9b1e65a3274b931d
 
 
 
@@ -11,9 +8,6 @@ print('Hello my name is Adiltai')
 
 
 
-<<<<<<< HEAD
-print('Hello my name is ADILTAI!')
-=======
 
 print('Hello my name is Islam')
 
@@ -29,4 +23,3 @@ print('Hello my name is Nurmuhamed.')
 print('Hello my name is Aibek!')
 print('My name is Muha')
 
->>>>>>> a230cdaed82c88bf6badeb3e9b1e65a3274b931d
