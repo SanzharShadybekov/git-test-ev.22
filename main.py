53c6ca455513983db43644a4292cf33694ea1cc9
sentinel20 <emil.begaliev@gmail.com>
2022-07-07 20:06:50 +0600

    adas

---
+++ main.py
@@ -0,0 +1,38 @@
+print('Hello my name is Sanzhar!')
+print("hello tima!!")
+
+
+print("hello tima!!")
+
+
+print('Hi, my name is Janylai!')
+print('Raimbek')
+
+print('Hello Im Ramazan')
+
+print('Hello Bema!!!')
+
+
+
+
+print('Hello Emil')
+print('My name is Igor!')
+print('My name is Tariel')
+
+print('Hello my name is Talgat')
+
+print('Hello my name is Islam')
+
+
+
+
+print('Hello my name is Nurmuhamed.')
+
+print('Hello my name is Manas')
+
+
+print('My name Tariel')
+print('Hello my name is Aibek!')
+print('My name is Muha')
+print('My name is Adiltay')
+
